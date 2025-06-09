089da34d1cab469df3b96a6f6f2f088e1b3f800b
dejithedev <onesixteenj@gmail.com>
2025-06-09 12:10:45 +0100

    refactor: update cookie settings to use environment variable for domain and secure flag

--- utils/generateAuthToken.js
+++ utils/generateAuthToken.js
@@ -9,7 +9,8 @@ export const generateAuthToken = (userKey, email, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         httpOnly: true,
         sameSite: 'none',
-        secure: true, // Requires HTTPS in production
+        domain: process.env.COOKIE_DOMAIN || 'localhost', // Set your domain here
+        secure: process.env.NODE_ENV === 'production', // Requires HTTPS in production
     });
 
     return token;
